2b0e3c7498f0a71a6285b548b8def39f465f5e4f
Elenagas <ele.unreall@gmail.com>
2019-09-01 01:22:29 +0200

    revised script with defined function of hsmetrics dictionary

--- scripts/parse_hsmetrics.py
+++ scripts/parse_hsmetrics.py
@@ -5,41 +5,67 @@ import glob
 import os
 import csv
 
-# Definition of required arguments (input_file path , output path) with argparse module: 
+######################
+##Function_Arguments##
+######################
+ 
 
 def check_arg(args=None):
-    parser = argparse.ArgumentParser(prog = 'script_dic_hsmetrics_v0.2.py',
+
+    '''
+	Description:
+        Function collect arguments from command line using argparse
+    Input:
+        args # command line arguments
+    Constant:
+        None
+    Variables
+        parser
+    Return
+        parser.parse_args() # Parsed arguments
+    '''
+
+    parser = argparse.ArgumentParser(prog = 'parse_hsmetrics_v0.2.py',
                                      formatter_class=argparse.RawDescriptionHelpFormatter, 
                                      description= 'Dictionary of hsmetrics_data from hsmetrics.out file')
 
     
     parser.add_argument('-v, --version', action='version', version='v0.2')
     parser.add_argument('--input', required= True, nargs="+",
-                                    help = 'Directory where hsMetrics.out files are stored.')
+                                    help = 'hsMetrics.out files incluiding path where files are stored.')
     parser.add_argument('--out',  required= True,
                                     help = 'csv file name incluiding path where the csv file will be stored.')
 
 
     return parser.parse_args()
 
-if __name__ == '__main__' :
 
-    arguments = check_arg(sys.argv[1:])
-    print('Arguments used: ' , arguments)
+########################
+##Function_Dictionary ##
+########################
 
-    #Dictionary of hsMetrics.out data:
+def hsmetrics_dictionary (files_out_input):
+    '''
+    Description:
+     'Dictionary of hsmetrics_data from hsmetrics.out file'
+    
+    Input:
+     hsMetrics.out file including path where is stored.' 
+    
+    Return:
+      d (dictionary)
+           
+    '''
+    
     d={}
-    for file in arguments.input :
+    for file in files_out_input :
         sample = (os.path.basename(file)).split('_')
         sample = sample[0]
-        #print('sample:', sample)
         found_start=False
         found_value=False
         d[sample]={}
-        #print('Dictionary samples', d)
-
+      
         with open(file) as hsmetrics_file:
-            #print('type_file', type(hsmetrics_file))
 
             for line in hsmetrics_file:
                 line=line.strip('\n')
@@ -47,7 +73,7 @@ if __name__ == '__main__' :
                     continue
                 if 'METRICS CLASS' in line :
                     found_start = True
-                    print('encuentre start:' , found_start)
+                    #print('found start:' , found_start)
                     continue
                 if found_start :
                     keys = line.split('\t')
@@ -58,40 +84,107 @@ if __name__ == '__main__' :
                     values=line.split('\t')
                     break
             hs_keys = ["hsMetrics_" + key for key in keys ]
-            print('hskeys', hs_keys)
+            #print('hskeys', hs_keys)
            
             for i in range(len(hs_keys)):
                 d[sample][hs_keys[i]]= values[i]
                 
-                #convert string to numbers
+                #convert string to numbers:
                 for val in d[sample][hs_keys[i]] :
                     try:
                         d[sample][hs_keys[i]]= float(values[i])
                     except (ValueError, TypeError):
                         d[sample][hs_keys[i]]= values[i]
 
-        #print(len(hs_keys), '-', len(values))
-                    
-        print('Dictionary_hsmetrics:' , d)
-
-        #Export dictionary as csv file:
-
-        outfile = arguments.out
-        dic = d #Nested dictionary
-
-        headers = list(list (dic.values())[0].keys()) #Encabezado de las columnas
-        #print (len (headers))
-        #print (headers)
         
-        with open(outfile, "w") as f:
-            w = csv.writer( f )
-            
-            w.writerow(['sample'] + headers)# printea la primera fila
-            
-            parameters = list(list (dic.values())[0].keys())
-            #print (parameters)
-            for sample in dic.keys():
-                #print (dic.keys())
-                w.writerow([sample] + [dic[sample][parameter] for parameter in parameters])
+    return(d)
+
+    
+######################
+##Function_Dic2CSV  ##
+######################   
+
+def dictionary2csv (dictionary, csv_file):
+
+    '''
+
+    Description:
+        Function to create a csv from a dictionary
+    Input:
+        dictionary
+    Return:
+        csv file
+
+   '''
+
+    header = sorted(set(i for b in map(dict.keys, dictionary.values()) for i in b))
+    with open(csv_file, 'w', newline="") as f:
+        write = csv.writer(f)
+        write.writerow(['sample', *header])
+        for a, b in dictionary.items():
+            write.writerow([a]+[b.get(i, '') for i in header])
+
+
+
+
+
+##########
+## MAIN ##
+########## 
+
+
+
+if __name__ == '__main__' :
+
+    
+    #Arguments: 
+    
+    '''
+    Example arguments:
+    --input /home/masterbioinfo/EXOME/Data/hsmetrics/*.out
+    --out /home/masterbioinfo/EXOME/Results/dic_hsMetrics_all.csv
+    '''
+    
+    arguments = check_arg(sys.argv[1:])
+    print('Arguments used: ' , arguments)
+
+
+
+    #Dictionary of hsMetrics.out data:
+    
+    dic_hsmetrics_all = hsmetrics_dictionary(arguments.input)
+    
+    print ('hsmetrics_dictionary done')
+    #print (dic_hsmetrics_all)
+    
+    
+    #Export dictionary as csv file:
+    
+    dictionary2csv (dic_hsmetrics_all, arguments.out)
+
+    print ('hsmetrics_csv done')
+    
+    
+    #Visualize CSV file using pandas:
+     
+    import pandas
+    panda_file = pandas.read_csv(arguments.out)
+    print(panda_file)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
